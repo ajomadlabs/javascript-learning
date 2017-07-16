f5e752612dc697c66e5843d6d809aac0da8afd8c
Ajo John <ajojohn555@gmail.com>
2017-07-16 19:09:30 +0530

    Added the number snippet

---
+++ numbers.js
@@ -0,0 +1,23 @@
+/* Numbers */
+
+/*
+With JavaScript numbers we can do all the matheatical operations ranging from 
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Modulos
+6. Increment 
+7. Decrement
+*/
+
+var myNumberOne = 12;
+var myNumberTwo = 13
+console.log(myNumberOne + myNumberTwo);
+console.log(myNumberOne - myNumberTwo);
+console.log(myNumberOne * myNumberTwo);
+console.log(myNumberOne / myNumberTwo);
+console.log(myNumberOne % myNumberTwo);
+console.log(myNumberOne++);
+console.log(myNumberOne);
+console.log(++myNumberOne);
