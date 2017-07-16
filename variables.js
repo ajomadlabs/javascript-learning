72e4253973011aa9ff7d8ffa33a16c00ea2d08aa
Ajo John <ajojohn555@gmail.com>
2017-07-16 16:58:11 +0530

    Modified the output

---
+++ variables.js
@@ -0,0 +1,24 @@
+/*Variables*/
+
+/*
+Variables are containers which holds values.
+Values inside a variable can be a string, number, complex function and objects.
+With variables, values insidea container can be changed.
+In Javascript, variables can be declared using var, const and let.
+At first the variable declared.
+After which the variable is intialized with a value.
+*/
+var myName;
+myName = "Ajo John Mathai";
+/*
+Variables can also be declared and initialised at one go
+*/
+var state = "Kerala";
+/*
+Variables has some conventions.
+Variables can start with an alphabet or an underscore.
+It is preferred not to use an underscore at start for every variable.
+It is preferred to follow lower cammel case convention like 'myNameIs'.
+Javascript is a loosely typed language, so datatypes need not be mentioned explicitly.
+*/
+console.log(myName + '-' + state); 
