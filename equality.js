bcea10301dbc02d4bd0eb456a75b9ae90813040c
Ajo John <ajojohn555@gmail.com>
2017-07-20 06:49:54 +0530

    Added the equality snippet

---
+++ equality.js
@@ -0,0 +1,28 @@
+/* Equality */
+
+/*
+There are two types of equality.
+Abstract Equaliy and Strict Equality.
+Abstract Equality - It is an equality which only checks the value and not the type.
+Strict Equality - It is an equality which checks both the type and the value.
+Abstract equality is denoted by '=='.
+Strict Equality is denoted by '==='.
+*/
+
+if (1 == '1') {
+    console.log("Hurray ! It's correct");
+} else {
+    console.log("Oh ! This is not working");
+}
+
+if ( 1 === '1') {
+    console.log("Hurray ! This is again great");
+} else {
+    console.log("Oh ! This is not working");
+}
+
+if (1 === 1) {
+    console.log("Great ! This is how it works");
+} else {
+    console.log("This does'nt work at all");
+}
