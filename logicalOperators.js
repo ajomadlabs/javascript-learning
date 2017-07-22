6452adb91b5f06ed9ab705eae83e6da198e97fc7
Ajo John <ajojohn555@gmail.com>
2017-07-22 06:25:20 +0530

    Added the Logical Operator and Short Circuit Evaluation

---
+++ logicalOperators.js
@@ -0,0 +1,40 @@
+/* Logical Operators and Short Circuit Evaluation */
+
+/*
+Logical Operators are mainly 
+1. AND &&
+2. OR ||
+AND is an operator which checks if both the statements are true.
+OR is an operator which check if either one of the statements is true.
+*/
+
+num = 6;
+
+if (num > 5 && num < 10) {
+    console.log("Yeah ! I found AND");
+} else {
+    console.log("I guess I need to grow more");
+}
+
+if (num > 5 || num < 10) {
+    console.log("Yeah ! I found OR");
+} else {
+    console.log("I guess I need to grow more");
+}
+
+/* Short Ciruit Evaluation */
+
+test = true;
+var newTrue = function() {
+    return true;
+}
+
+var newFalse = function() {
+    return false;
+}
+
+if (test && newTrue()) {
+    console.log("Yeah I'm true");
+} else {
+    console.log("Yeah I'm false");
+}
