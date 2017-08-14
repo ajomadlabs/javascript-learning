9e5fc1716ed11e22fceda6796c538511009f718a
Ajo John <ajojohn555@gmail.com>
2017-08-14 23:55:38 +0530

    Added Promises snippet

---
+++ promises.js
@@ -0,0 +1,11 @@
+/* Promises */
+
+/*
+These are placeholders or proxies that we may not
+know when the promises are created.
+We don't know when an asyn task finishes in our programe.
+Promises allows us to do some tasks when an asyn process 
+does finish.
+Instead of returnning the final value, asyn returns a 
+promise.
+*/
